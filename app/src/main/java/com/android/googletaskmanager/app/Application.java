bdaef67d5defacdfc37474bb3fad2e99c1d688fe
lorence <lorence@enclave.vn>
2019-09-04 17:24:53 +0700

    Optimize code

---
+++ app/src/main/java/com/android/googletaskmanager/app/Application.java
@@ -0,0 +1,10 @@
+package com.android.googletaskmanager.app;
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
